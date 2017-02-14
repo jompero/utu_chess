0843bbf8185b7c9f3d9653e3b95c710b7810a889
jompero <Dani Jompero@Jompero>
2017-02-14 11:44:46 +0200

    Visual changes

--- Chessgame/src/chessgame/Check.java
+++ Chessgame/src/chessgame/Check.java
@@ -1,31 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chessgame;
 
 import java.awt.Point;
+
+import com.sun.prism.shape.ShapeRep.InvalidationType;
 
 import javafx.scene.effect.InnerShadow;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 
-/**
- *
- * @author Dani Jompero
- */
 class Check extends Rectangle {
     final Color paint;
     final Color dark = Color.DARKGRAY;
     final Color light = Color.LIGHTGRAY;
-    final Color highlight = Color.YELLOW;
-    final int strokeWeight = 2;
+    final Color highlight = Color.WHITE;
+    final int strokeWeight = 4;
     InnerShadow shadow = new InnerShadow(4, 2, 2, Color.BLACK);
     InnerShadow noShadow = new InnerShadow(0, 0, 0, Color.BLACK);
-    InnerShadow glow = new InnerShadow(10, 0, 0, Color.CYAN);
-    InnerShadow noGlow = new InnerShadow(0, 0, 0, Color.YELLOW);
     
     
     public Check(Point p) {
@@ -33,17 +24,13 @@ class Check extends Rectangle {
         
         paint = ((p.getX() + p.getY()) % 2) == 0 ? dark : light;
         this.setFill(paint);
+        this.setStroke(highlight);
         this.setStrokeType(StrokeType.INSIDE);
         this.setStrokeWidth(0);
-        this.setStroke(highlight);
     }
     
     public void highlightFX(boolean isOn) {
-    	if (isOn) {
-    		this.setEffect(glow);
-    	} else {
-    		this.setEffect(noGlow);
-    	}
+    	this.setStrokeWidth(isOn ? strokeWeight : 0);
     }
     
     public void selectFX(boolean isOn) {
