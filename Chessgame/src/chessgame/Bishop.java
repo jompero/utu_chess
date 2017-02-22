815d3ce581048899686c4ed14ee5e9c69836d3ca
Olympos <Olympos@Ares>
2017-02-22 14:04:54 +0200

    Cleaned code.

--- Chessgame/src/chessgame/Bishop.java
+++ Chessgame/src/chessgame/Bishop.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chessgame;
 
 import java.awt.Point;
 import java.util.ArrayList;
 import javafx.scene.image.Image;
 
-/**
- *
- * @author Dani Jompero
- */
+
 public class Bishop extends Piece {
     
     String spriteWhite = "img/bw.png";
--- Chessgame/src/chessgame/King.java
+++ Chessgame/src/chessgame/King.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chessgame;
 
 import java.awt.Point;
 import java.util.ArrayList;
 import javafx.scene.image.Image;
 
-/**
- *
- * @author Dani Jompero
- */
 public class King extends Piece {
     String spriteWhite = "img/kw.png";
     String spriteBlack = "img/kb.png";
